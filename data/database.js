c960f93c74988a6ef39605595b9c3a7a55b4b182
imran00852 <bhatimran9797@gmail.com>
2024-08-24 13:30:17 +0530

    updated

--- data/database.js
+++ data/database.js
@@ -1,11 +1,11 @@
-import mysql from "mysql2";
+import mysql from "mysql2/promise";
 import { config } from "dotenv";
 
 config({
   path: "./data/config.env",
 });
 
-export const connection = mysql.createConnection({
+export const connection = await mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   database: process.env.MYSQL_DATABASE,
